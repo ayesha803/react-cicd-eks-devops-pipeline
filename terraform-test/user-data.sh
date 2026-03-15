d6bd2ae4ce237d7796ec9ebfa12d0ac968f2967b
ayesha803 <81418736+ayesha803@users.noreply.github.com>
2026-03-15 14:18:27 +0530

    Update user-data.sh

--- terraform-test/user-data.sh
+++ terraform-test/user-data.sh
@@ -39,6 +39,13 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
+
+#eksctl 
+curl --silent --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+
+
 # Enable and start Jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
